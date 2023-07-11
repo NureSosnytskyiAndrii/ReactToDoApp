b0b7d50bab39ce7e4564d34bd874d58d0ea9e3ad
annndre <102580124+annndre@users.noreply.github.com>
2023-07-11 15:11:13 +0300

    Update TodoWrapper.js

--- todoapp/src/components/TodoWrapper.js
+++ todoapp/src/components/TodoWrapper.js
@@ -44,7 +44,7 @@ export const TodoWrapper = () => {
     return (
         <div className="TodoWrapper">
             <DarkMode />
-            <h1>Get Things Done !</h1>
+            <h1>ToDo Application</h1>
             <TodoForm addTodo={addTodo} />
             {/* display todos */}
             {todos.map((todo) =>
